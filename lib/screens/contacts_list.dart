9cd491a1344d06724502e9e10adc43bc9e8b937d
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-06-17 13:42:16 -0300

    Finishing with different build and listeners

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -84,11 +84,7 @@ class ContactsList extends StatelessWidget {
                 return _ContactItem(
                   contact,
                   onClick: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => TransactionForm(contact),
-                      ),
-                    );
+                    push(context, TransactionFormContainer(contact));
                   },
                 );
               },
